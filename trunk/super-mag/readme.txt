e0192a8ed628b2b6dc1ec40751bda249bcc30971
sandeep.maloth@gmail.com <sandeep.maloth@gmail.com@768fc377-1c2c-ca98-b545-07018d626cd3>
2014-02-02 21:51:49 +0000

    added

---
+++ trunk/super-mag/readme.txt
@@ -0,0 +1,2 @@
+Gotchas:
+1. copy the libs to web-inf/lib the first time by build mvn package and pick libraries from target.
---
+++ trunk/super-mag/src/com/bri8/supermag/dao/BaseDAO.java
@@ -0,0 +1,128 @@
+package com.bri8.supermag.dao;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+import static com.bri8.supermag.util.Constants.*;
+
+public class BaseDAO<T> {
+
+	private static Log logger = LogFactory.getLog(BaseDAO.class);
+
+	public T create(T entity) {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		try {
+			pm.makePersistent(entity);
+		} finally {
+			pm.flush();
+			pm.close();
+		}
+		return entity;
+	}
+
+	@SuppressWarnings("unchecked")
+	public T update(T entity, Class clasz) {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		try {
+			pm.makePersistent(entity);
+		} finally {
+			pm.flush();
+			pm.close();
+		}
+		return entity;
+
+	}
+
+	/**
+	 * Use this to avoid * obj is managed by a different Object Manager
+	 */
+	@SuppressWarnings("unchecked")
+	public T update(T entity, Long id, Class clasz) {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		T entityToUpdate = read(id, clasz, pm);
+		try {
+			BeanUtils.copyProperties(entityToUpdate, entity);
+			pm.makePersistent(entityToUpdate);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			pm.flush();
+			pm.close();
+		}
+		return entityToUpdate;
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public void delete(Long id, Class clasz) {
+		T entity = read(id, clasz);
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		try {
+			pm.deletePersistent(entity);
+		} finally {
+			pm.flush();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> read(Class clasz, int count) {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		javax.jdo.Query q = pm.newQuery("select from " + clasz.getName() + " ");
+		q.setRange(0, count);
+		List<T> entries = (List<T>) q.execute();
+		logger.debug(entries);
+		return entries;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> read(Class clasz, String where, String orderBy, int count) {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		javax.jdo.Query q = pm.newQuery("select from " + clasz.getName() + " where " + where + SPACE + orderBy);
+		q.setRange(0, count);
+		List<T> entries = (List<T>) q.execute();
+		logger.debug(entries);
+		return entries;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> read(Class clasz, String where, List<String> contains, String orderBy, int count) {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		javax.jdo.Query q = pm.newQuery("select from " + clasz.getName() + " where " + where + SPACE + orderBy);
+		q.setRange(0, count);
+		List<T> entries = (List<T>) q.execute(contains);
+		logger.debug(entries);
+		return entries;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> read(Class clasz, String where, String orderBy) {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		javax.jdo.Query q = pm.newQuery("select from " + clasz.getName() + SPACE + " where " + where + SPACE + orderBy);
+		List<T> entries = (List<T>) q.execute();
+		logger.debug(entries);
+		return entries;
+	}
+
+	@SuppressWarnings("unchecked")
+	public T read(Long id, Class clasz) {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		return read(id, clasz, pm);
+	}
+
+	@SuppressWarnings("unchecked")
+	public T read(Long id, Class clasz, PersistenceManager pm) {
+		Key k = KeyFactory.createKey(clasz.getSimpleName(), id);
+		T q = (T) pm.getObjectById(clasz, k);
+		return q;
+	}
+
+}
