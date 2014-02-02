7d51245dcc8f4f65eb4d1bc743b25d1c1d1e48f9
sandeep.maloth@gmail.com <sandeep.maloth@gmail.com@768fc377-1c2c-ca98-b545-07018d626cd3>
2014-02-02 21:51:49 +0000

    added

---
+++ super-mag/src/com/bri8/supermag/util/Constants.java
@@ -0,0 +1,140 @@
+package com.bri8.supermag.util;
+
+public interface Constants {
+
+	String AMPERSAND = "&";
+
+	String BACK_SLASH = "\\";
+
+	String BLANK = "";
+
+	String CLOSE_BRACKET = "]";
+
+	String CLOSE_CURLY_BRACE = "}";
+
+	String CLOSE_PARENTHESIS = ")";
+
+	String COLON = ":";
+
+	String COMMA = ",";
+
+	String COMMA_AND_SPACE = ", ";
+
+	String DASH = "-";
+
+	String DOUBLE_APOSTROPHE = "''";
+
+	String DOUBLE_CLOSE_BRACKET = "]]";
+
+	String DOUBLE_OPEN_BRACKET = "[[";
+
+	String DOUBLE_SLASH = "//";
+
+	String EQUAL = "=";
+
+	String GREATER_THAN = ">";
+
+	String GREATER_THAN_OR_EQUAL = ">=";
+
+	String FALSE = "false";
+
+	String FORWARD_SLASH = "/";
+
+	String FOUR_SPACES = "    ";
+
+	String IS_NOT_NULL = "IS NOT NULL";
+
+	String IS_NULL = "IS NULL";
+
+	String LESS_THAN = "<";
+
+	String LESS_THAN_OR_EQUAL = "<=";
+
+	String LIKE = "LIKE";
+
+	String MINUS = "-";
+
+	String NBSP = "&nbsp;";
+
+	String NEW_LINE = "\n";
+
+	String NOT_EQUAL = "!=";
+
+	String NOT_LIKE = "NOT LIKE";
+
+	String NULL = "null";
+
+	String OPEN_BRACKET = "[";
+
+	String OPEN_CURLY_BRACE = "{";
+
+	String OPEN_PARENTHESIS = "(";
+
+	String PERCENT = "%";
+
+	String PERIOD = ".";
+
+	String PIPE = "|";
+
+	String PLUS = "+";
+
+	String POUND = "#";
+
+	String QUESTION = "?";
+
+	String QUOTE = "\"";
+
+	String RETURN = "\r";
+
+	String RETURN_NEW_LINE = "\r\n";
+
+	String SEMICOLON = ";";
+
+	String SLASH = FORWARD_SLASH;
+
+	String SPACE = " ";
+
+	String STAR = "*";
+
+	String TAB = "\t";
+
+	String TILDE = "~";
+
+	String TRUE = "true";
+
+	String UNDERLINE = "_";
+
+	String UTF8 = "UTF-8";
+
+	String TEXT_XML = "text/xml";
+
+	int SECOND = 1000;
+
+	String LUCENE_DIR = "lucene.dir";
+
+	String LUCENE_STORE_TYPE = "lucene.store.type";
+
+	String LUCENE_ANALYZER = "lucene.analyzer";
+
+	String MODIFIED = "modified";
+
+	String HTTP_WITH_SLASH = "http://";
+
+	String HTTPS_WITH_SLASH = "https://";
+
+	String SPRING_CONFIG_LOCATIONS[] = { "applicationContext.xml" };
+
+	boolean SORTABLE = true;
+
+	int SORTABLE_FIELD_MAX_SIZE = 20;
+
+	char[] LUCENE_SANITIZE_CHARS = { ':' };
+
+	String VIEW_LAYOUT_LAYOUT_VM = "layout/layout.vm";
+
+	int MAX_QUERY_RESULT = 1000;
+	
+	String EBAY_URL_DEFAULT = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${appname}&GLOBAL-ID=EBAY-US&keywords=${keywords}&paginationInput.entriesPerPage=30";
+	
+	String LINE="------------------------------------";
+}
---
+++ super-mag/src/com/bri8/supermag/web/common/StaticPageViewController.java
@@ -0,0 +1,61 @@
+package com.bri8.supermag.web.common;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bri8.supermag.util.Constants;
+import com.bri8.supermag.web.view.CommonVelocityLayoutView;
+
+/**
+ * Simple controller that takes as inputs a LayoutView and a content pagename and puts them together
+ */
+@Controller("staticPageViewController")
+public class StaticPageViewController {
+	
+	private static final String TITLE_PREFIX = "title.";
+	private String pageParam = "page";
+	@Autowired
+	CommonVelocityLayoutView layoutView;
+
+	private static Log logger = LogFactory.getLog(StaticPageViewController.class);
+
+	@RequestMapping(value = "/static", method = RequestMethod.GET)
+	public ModelAndView handleRequest(@RequestParam("page") String page) throws Exception {
+		String propName = TITLE_PREFIX + page;
+		logger.info("Looking for title: " + propName);// TODO:
+
+		ModelAndView mav = getStaticModelAndView(page);
+		return mav;
+	}
+
+	protected ModelAndView getStaticModelAndView(String page) {
+		return getModelAndView(page);
+	}
+
+	private ModelAndView getModelAndView(String page) {
+		ModelAndView mav = new ModelAndView();
+
+		layoutView.setUrl(page);
+		layoutView.setLayoutUrl(Constants.VIEW_LAYOUT_LAYOUT_VM);
+
+		mav.setView(layoutView);
+		mav.addAllObjects(layoutView.getIncludes());
+		mav.addObject("title", "TODO:");
+		return mav;
+	}
+
+	public String getPageParam() {
+		return pageParam;
+	}
+
+	public void setPageParam(String pageVar) {
+		this.pageParam = pageVar;
+	}
+
+}
