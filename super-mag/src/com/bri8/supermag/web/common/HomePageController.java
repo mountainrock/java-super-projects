04460075e8a5393668553bff89e1407ed9d3b08a
sandeep.maloth@gmail.com <sandeep.maloth@gmail.com@768fc377-1c2c-ca98-b545-07018d626cd3>
2014-02-04 21:35:03 +0000

    added login/register links

--- super-mag/src/com/bri8/supermag/web/common/HomePageController.java
+++ super-mag/src/com/bri8/supermag/web/common/HomePageController.java
@@ -1,15 +1,19 @@
-package com.bri8.supermag.web.command;
+package com.bri8.supermag.web.common;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bri8.supermag.util.Constants;
+import com.bri8.supermag.web.BaseController;
+
 @Controller
-public class HomePageController {
+public class HomePageController extends BaseController{
 
 	@RequestMapping(value = { "/", "/index", "/home" }, method = RequestMethod.GET)
 	protected ModelAndView show() throws Exception {
-		return new ModelAndView("index");
+		return getDefaultModelAndView("index");
 	}
+	
 }
