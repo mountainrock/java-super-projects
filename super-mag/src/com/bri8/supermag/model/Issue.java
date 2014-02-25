ce5fceaf20bbb0aa5062086db7263afbcac33170
sandeep.maloth@gmail.com <sandeep.maloth@gmail.com@768fc377-1c2c-ca98-b545-07018d626cd3>
2014-02-25 22:42:55 +0000

    handling issue upload

--- super-mag/src/com/bri8/supermag/model/Issue.java
+++ super-mag/src/com/bri8/supermag/model/Issue.java
@@ -1,90 +1,116 @@
 package com.bri8.supermag.model;
 
-import javax.jdo.annotations.IdGeneratorStrategy;
-import javax.jdo.annotations.IdentityType;
+import java.util.Date;
+
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
-import javax.jdo.annotations.PrimaryKey;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
 
-@PersistenceCapable(identityType = IdentityType.APPLICATION)
+@PersistenceCapable
 public class Issue extends BaseModel {
-	 @PrimaryKey
-	 @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
-	 Long issueId;
-	 
-	 @Persistent Long  magazineId;
-	 @Persistent String  issueName;
-	 @Persistent String  description;
-	 @Persistent String  status;// (preview, review, live)
-	 @Persistent Boolean  specialIssue;
-	 @Persistent String  publishingDate;
-	 @Persistent String  previewPages;
-	 @Persistent Float  singleIssuePrice;
-	 @Persistent String  blobKey;
-	 
+	@Persistent
+	Long issueId;
+
+	@Persistent
+	Magazine magazine;
+	@Persistent
+	String issueName;
+	@Persistent
+	String description;
+	@Persistent
+	String status;// (preview, review, live)
+	@Persistent
+	Boolean specialIssue;
+	@Persistent
+	Date publishingDate;
+	@Persistent
+	String previewPages;
+	@Persistent
+	Float singleIssuePrice;
+	@Persistent
+	String blobKey;
+
 	public Long getIssueId() {
 		return issueId;
 	}
+
 	public void setIssueId(Long issueId) {
 		this.issueId = issueId;
 	}
-	public Long getMagazineId() {
-		return magazineId;
+
+	public Magazine getMagazine() {
+		return magazine;
 	}
-	public void setMagazineId(Long magazineId) {
-		this.magazineId = magazineId;
+
+	public void setMagazine(Magazine magazine) {
+		this.magazine = magazine;
 	}
+
 	public String getIssueName() {
 		return issueName;
 	}
+
 	public void setIssueName(String issueName) {
 		this.issueName = issueName;
 	}
+ 
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
 	public Boolean getSpecialIssue() {
 		return specialIssue;
 	}
+
 	public void setSpecialIssue(Boolean specialIssue) {
 		this.specialIssue = specialIssue;
 	}
-	public String getPublishingDate() {
+
+	public Date getPublishingDate() {
 		return publishingDate;
 	}
-	public void setPublishingDate(String publishingDate) {
+
+	public void setPublishingDate(Date publishingDate) {
 		this.publishingDate = publishingDate;
 	}
+
 	public String getPreviewPages() {
 		return previewPages;
 	}
+
 	public void setPreviewPages(String previewPages) {
 		this.previewPages = previewPages;
 	}
+
 	public Float getSingleIssuePrice() {
 		return singleIssuePrice;
 	}
+
 	public void setSingleIssuePrice(Float singleIssuePrice) {
 		this.singleIssuePrice = singleIssuePrice;
 	}
+
 	public String getBlobKey() {
 		return blobKey;
 	}
+
 	public void setBlobKey(String blobKey) {
 		this.blobKey = blobKey;
 	}
-	
+
 	@Override
 	public String toString() {
 		return ToStringBuilder.reflectionToString(this);
--- super-mag/src/com/bri8/supermag/web/MagazineController.java
+++ super-mag/src/com/bri8/supermag/web/MagazineController.java
@@ -6,10 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bri8.supermag.model.Issue;
 import com.bri8.supermag.model.Magazine;
 import com.bri8.supermag.model.User;
 import com.bri8.supermag.service.MagazineService;
@@ -44,9 +46,30 @@ public class MagazineController extends BaseController{
 	@RequestMapping(value = { "/magazine/list" }, method = RequestMethod.GET)
 	protected ModelAndView list(HttpServletRequest request) throws Exception {
 		User user = (User) request.getSession().getAttribute("user");
-		List<Magazine> magazines = magazineService.listMagazine(user.getUserId());
+		List<Magazine> magazines = magazineService.listMagazine(user.getUserId(),true);
 		ModelAndView mv = getDefaultModelAndView("magazine/list");
 		mv.addObject("magazines", magazines);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value = { "/magazine/showAddIssue/{magazineId}" }, method = RequestMethod.GET)
+	protected ModelAndView showAddIssue(@PathVariable("magazineId") Long magazineId) throws Exception {
+		ModelAndView mv = getDefaultModelAndView("magazine/issue/showAddIssue");
+		mv.addObject("magazineId", magazineId);
+		return mv;
+	}
+	
+	@RequestMapping(value = { "/magazine/createIssue" }, method = RequestMethod.POST)
+	public ModelAndView createIssue(Issue issue, HttpServletRequest request) {
+		magazineService.createIssue(issue);
+		ModelAndView mv = getDefaultModelAndView("magazine/showAdd");
+		if(issue!=null && issue.getIssueId()!=null){
+			mv.addObject("message", "Issue created sucessfully : "+ issue.getIssueId());
+			mv.addObject("issue",issue);
+		}else{
+			mv.addObject("error", "Failed to create issue!!");
+		}
 		
 		return mv;
 	}
