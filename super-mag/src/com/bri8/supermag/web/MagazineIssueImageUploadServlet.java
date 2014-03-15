aae82cff8279364e7ccd3ec54487149a18c94d46
sandeep.maloth@gmail.com <sandeep.maloth@gmail.com@768fc377-1c2c-ca98-b545-07018d626cd3>
2014-03-15 22:13:33 +0000

    added converter

---
+++ super-mag/src/com/bri8/supermag/web/MagazineIssueImageUploadServlet.java
@@ -0,0 +1,40 @@
+package com.bri8.supermag.web;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.bri8.supermag.service.MagazineService;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+@SuppressWarnings("serial")
+public class MagazineIssueImageUploadServlet extends HttpServlet {
+	private final static Logger logger = Logger.getLogger(MagazineIssueImageUploadServlet.class.getName());
+	private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		ApplicationContext springContext = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+		MagazineService magazineService = (MagazineService)springContext.getBean("magazineService");
+		Map<String, BlobKey> blobs = blobstoreService.getUploadedBlobs(req);
+		logger.info("inside upload servlet blobs : " + blobs + " , params : "+ req.getParameterMap());
+		BlobKey blobKey = blobs.get("issueImageFile");
+		String magazineId = req.getParameter("magazineId");
+		String issueId = req.getParameter("issueId");
+	//TODO: update issue images in new entitye MagazineIssueImage
+		//	magazineService.updateIssueBlobKey(Long.parseLong(issueId), blobKey.getKeyString());
+		
+		logger.info("Blob key : "+ blobKey.getKeyString() +", magazineId :"+ magazineId+ ", issueId :"+ issueId);
+		res.sendRedirect("/magazine/list");
+	}
+}
