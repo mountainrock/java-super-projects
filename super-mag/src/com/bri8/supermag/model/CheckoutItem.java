87058bf06418344dd1ec14169d9ac611c720f6ac
sandeep.maloth@gmail.com <sandeep.maloth@gmail.com@768fc377-1c2c-ca98-b545-07018d626cd3>
2015-01-28 09:05:42 +0000

    checkout

--- super-mag/src/com/bri8/supermag/model/CheckoutItem.java
+++ super-mag/src/com/bri8/supermag/model/CheckoutItem.java
@@ -1,7 +1,32 @@
 package com.bri8.supermag.model;
 
-public class CheckoutItem {
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+/**
+ * used in shopping basket
+ *
+ */
+@PersistenceCapable(detachable = "true")
+public class CheckoutItem extends BaseModel {
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	Long itemId;
+
+	@Persistent
+	Long userId;
+
+	@Persistent
+	Long issueId;
+
+	@Persistent
 	String issueName;
+
+	@Persistent
 	String subscriptionType;
 
 	public String getIssueName() {
@@ -20,9 +45,32 @@ public class CheckoutItem {
 		this.subscriptionType = subscriptionType;
 	}
 
+	public Long getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(Long itemId) {
+		this.itemId = itemId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Long getIssueId() {
+		return issueId;
+	}
+
+	public void setIssueId(Long issueId) {
+		this.issueId = issueId;
+	}
+
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
+		return ToStringBuilder.reflectionToString(this);
 	}
 }
