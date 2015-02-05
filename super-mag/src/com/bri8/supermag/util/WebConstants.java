04663080570aeaae54a4f6a696f0508f541a866a
sandeep.maloth@gmail.com <sandeep.maloth@gmail.com@768fc377-1c2c-ca98-b545-07018d626cd3>
2015-02-05 07:54:15 +0000

    added

--- super-mag/src/com/bri8/supermag/util/WebConstants.java
+++ super-mag/src/com/bri8/supermag/util/WebConstants.java
@@ -3,4 +3,7 @@ package com.bri8.supermag.util;
 
 public interface WebConstants {
 	String HTTP_SESSION_KEY_USER="user";
+	String USER_TYPE_PUBLISHER="publisher";
+	String USER_TYPE_SUBSCRIBER="subscriber";
+	String USER_TYPE_ADMIN="admin";
 }
