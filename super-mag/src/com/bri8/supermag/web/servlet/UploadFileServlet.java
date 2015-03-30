49dea41c932d2c8c273439c63b66b28baf605252
mountainrock <sandeep.maloth@gmail.com>
2015-03-30 12:44:09 +0530

    upload with gcs client

--- super-mag/src/com/bri8/supermag/web/servlet/UploadFileServlet.java
+++ super-mag/src/com/bri8/supermag/web/servlet/UploadFileServlet.java
@@ -1,11 +1,15 @@
 package com.bri8.supermag.web.servlet;
+
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.channels.Channels;
+import java.util.Date;
+import java.util.logging.Logger;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,8 +17,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.fileupload.FileItemIterator;
 import org.apache.commons.fileupload.FileItemStream;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.IOUtils;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import com.bri8.supermag.model.IssuePage;
+import com.bri8.supermag.service.MagazineService;
 import com.bri8.supermag.util.WebConstants;
+import com.bri8.supermag.web.MagazineIssueImageUploadServlet;
 import com.google.appengine.api.images.Image;
 import com.google.appengine.api.images.ImagesService;
 import com.google.appengine.api.images.ImagesServiceFactory;
@@ -26,7 +36,6 @@ import com.google.appengine.tools.cloudstorage.GcsService;
 import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
 import com.google.appengine.tools.cloudstorage.RetryParams;
 
-
 public class UploadFileServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -35,25 +44,46 @@ public class UploadFileServlet extends HttpServlet {
 	public static final String BUCKET_NAME = WebConstants.BUCKETNAME;
 	private final GcsService gcsService = GcsServiceFactory.createGcsService(RetryParams.getDefaultInstance());
 	private ImagesService imagesService = ImagesServiceFactory.getImagesService();
-	
+	private final static Logger log = Logger.getLogger(MagazineIssueImageUploadServlet.class.getName());
+	private MagazineService magazineService;
+
 	@Override
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	public void init() throws ServletException {
+		ApplicationContext springContext = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+		magazineService = (MagazineService) springContext.getBean("magazineService");
+	}
 
-			resp.setContentType("text/plain");
-			resp.getWriter().println("Now see here your file content, that you have uploaded on storage..");
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse response) throws IOException {
 
-			ServletFileUpload upload = new ServletFileUpload();
-			FileItemIterator iter;
-			try {
-				iter = upload.getItemIterator(req);
-				while (iter.hasNext()) {
-					FileItemStream item = iter.next();
-					if(item.isFormField()){
-						continue;
-					}
-					String fileName = item.getName();
-					String mime = item.getContentType();
+		response.setContentType("text/plain");
+
+		ServletFileUpload upload = new ServletFileUpload();
+		FileItemIterator iterator;
+		String magazineId = null;
+		String issueId = null;
+		try {
+			iterator = upload.getItemIterator(req);
+			int pageNumber = 1;
+			while (iterator.hasNext()) {
+				FileItemStream item = iterator.next();
+				InputStream stream = item.openStream();
+
+				if (item.isFormField()) {
+					String fieldName = item.getFieldName();
 					
+					if ("magazineId".equals(fieldName)) {
+						magazineId = IOUtils.toString(stream);
+						log.warning(String.format("Form field: %s = %s ",fieldName, magazineId));
+					} else if ("issueId".equals(fieldName)) {
+						issueId = IOUtils.toString(stream);
+						log.warning(String.format("Form field: %s = %s ",fieldName, issueId));
+					}
+				} else {
+					log.info("Saving images ");
+					String fileName = String.format("%s-%s-%s-%s", magazineId, issueId, new Date().getTime(), item.getName());
+					String mime = item.getContentType();
+
 					GcsFileOptions options = new GcsFileOptions.Builder().acl("public_read").mimeType(mime).build();
 					GcsFilename filename = new GcsFilename(BUCKET_NAME, fileName);
 					GcsOutputChannel outputChannel = gcsService.createOrReplace(filename, options);
@@ -61,42 +91,54 @@ public class UploadFileServlet extends HttpServlet {
 					// Writing the file to input stream
 					InputStream is = new BufferedInputStream(item.openStream());
 					// Copying InputStream to GcsOutputChannel
-					Image oldImage=null;
+					Image oldImage = null;
 					try {
+						log.info("Saving image : " + filename);
 						OutputStream newOutputStream = Channels.newOutputStream(outputChannel);
 						oldImage = copy(is, newOutputStream);
 					} finally {
 						outputChannel.close();
 						is.close();
 					}
-					
-					GcsFilename fileNameThmb = new GcsFilename(BUCKET_NAME, "thmb-"+ fileName);
+
+					String fileNameThmbStr = String.format("%s-%s-%s-thmb-%s", magazineId, issueId, new Date().getTime(), item.getName());
+					GcsFilename fileNameThmb = new GcsFilename(BUCKET_NAME, fileNameThmbStr);
 					GcsOutputChannel outputChannelThmb = gcsService.createOrReplace(fileNameThmb, options);
-					
+
 					Transform resize = ImagesServiceFactory.makeResize(150, 200, true);
 					Image newImage = imagesService.applyTransform(resize, oldImage);
 					byte[] newImageData = newImage.getImageData();
 					try {
+						log.info("Saving thmb image : " + fileNameThmbStr);
 						OutputStream newOutputStream = Channels.newOutputStream(outputChannelThmb);
 						newOutputStream.write(newImageData);
 					} finally {
 						outputChannelThmb.close();
-						newImage= null;
+						newImage = null;
 					}
-					
-					resp.getWriter().println("File uploading done");
 
+					log.info(String.format(" Storing magazine issue page( File name : %s, magazineId :%s, issueId :%s)", fileName, magazineId, issueId));
+
+					IssuePage issuePage = new IssuePage();
+					issuePage.setIssueId(Long.parseLong(issueId));
+					issuePage.setPageNumber(pageNumber++);
+					long time = new Date().getTime();
+					String fileNameFull = String.format("%s-%s-%s-%s", magazineId, issueId, time, fileName);
+					issuePage.setFileName(fileNameFull);
+					issuePage.setFileNameThumbnail(fileNameThmbStr);
+					magazineService.addIssueImageBlobKey(issuePage);
+					// resp.getWriter().println("File uploading done");
 				}
-			} catch (Exception e) {
-				e.printStackTrace(resp.getWriter());
-				System.out.println("Exception::" + e.getMessage());
-				e.printStackTrace();
 			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		response.sendRedirect(String.format("/magazine/showUploadIssue/%s/%s", magazineId, issueId));
 	}
 
 	private Image copy(InputStream input, OutputStream output) throws IOException {
 		byte[] buffer = new byte[BUFFER_SIZE];
-		ByteArrayOutputStream baos= new ByteArrayOutputStream();
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		int bytesRead = input.read(buffer);
 		while (bytesRead != -1) {
 			output.write(buffer, 0, bytesRead);
